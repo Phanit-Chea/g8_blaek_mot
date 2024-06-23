c6eb6005fd327fe23f9d64724b2f963a65ce411c
RATANAK.KHOEURN <RATANAK.KHOEURN@PNC.LAN>
2024-06-23 10:43:17 +0700

    seperate navbar and footer to put in own folder #GBM3

--- frontend/src/views/Web/HomeView.vue
+++ frontend/src/views/Web/HomeView.vue
@@ -1,63 +1,6 @@
 <template>
   <div class="container h-100">
-    <header class="row bg-white p-2 fixed-top">
-      <div class="col-md-auto d-flex align-items-center text-green">
-        <img
-          src="../../assets/ContainerImages/logo.png"
-          alt="homeImage"
-          width="100%"
-          height="100px"
-        />
-        <!-- <h1 class="logo" style="font-family: 'Moul', serif; margin-left: 20px">ប្លែកមាត់</h1> -->
-      </div>
-      <div
-        class="col-md d-flex align-items-center"
-        style="width: 100%; margin-left: 3%; margin-top: 10px; margin-bottom: 10px"
-      >
-        <div class="input-group w-250">
-          <button class="input-group-text border border-success border-end-0">
-            <i class="material-icons">search</i>
-          </button>
-          <input
-            type="text"
-            class="form-control p-3 border border-success border-end-0 shadow-none"
-            placeholder="What would like to eat?"
-            aria-label="Search"
-          />
-          <button class="text-light input-group-text border border-success bg-green border-start-0">
-            Search
-          </button>
-        </div>
-      </div>
-      <div class="col-md d-flex justify-content-end align-items-center gap-4 m-2">
-        <a
-          href="#"
-          class="me-3 rounded-circle d-flex p-1 justify-content-center align-items-center bg-white text-decoration-none" style="border: 3px solid rgb(43, 212, 145)"
-        >
-          <i class="material-icons text-green fs-1">account_circle</i>
-        </a>
-        <a
-          href="#"
-          class="me-3 rounded-circle d-flex p-1 justify-content-center align-items-center bg-white text-decoration-none" style="border: 3px solid rgb(43, 212, 145)"
-        >
-          <i class="material-icons text-green fs-1">notifications</i>
-        </a>
-        <a
-          href="#"
-          class="me-3 rounded-circle d-flex p-1 justify-content-center align-items-center bg-white text-decoration-none" style="border: 3px solid rgb(43, 212, 145)"
-        >
-          <i class="material-icons text-green fs-1">turned_in_not</i>
-        </a>
-      </div>
-      <nav class="row text-warning mt-3">
-        <div class="col d-flex" style="gap: 20px; margin-left: 20px">
-          <a href="#" class="text-green text-decoration-none nav-link">Home</a>
-          <a href="#" class="text-green text-decoration-none nav-link">Category</a>
-          <a href="#" class="text-green text-decoration-none nav-link">About Us</a>
-          <a href="#" class="text-green text-decoration-none nav-link">Join With Us</a>
-        </div>
-      </nav>
-    </header>
+    <NavbarView></NavbarView>
     <main class="container" style="margin-top: 14%">
       <div class="containerTop">
         <img
@@ -583,99 +526,17 @@
         </div>
       </div>
     </main>
-    <div class="container m-2.8" style="background-color: #2a581a">
-      <footer class="text-light py-5">
-        <div class="col-md-auto d-flex align-items-center text-green">
-          <img
-            src="../../assets/FooterImages/logo.png"
-            alt="homeImage"
-            width="16%"
-            height="100px"
-          />
-          <!-- <h1 class="logo" style="font-family: 'Moul', serif; margin-left: 20px">ប្លែកមាត់</h1> -->
-        </div>
-        <hr />
-        <div class="container mt-3" style="margin-left: 20px">
-          <div class="row text-center text-md-start">
-            <div class="col-md mb-5">
-              <h3>About Us</h3>
-              <p>កកូរ</p>
-              <p>ស៊ុបគោ</p>
-              <p>ស៊ុបពពែរ</p>
-              <p>ស៊ុបត្រីឆ្លាម</p>
-              <p>ស៊ុមស៊ីឆាន់</p>
-            </div>
-            <div class="col-md mb-5">
-              <h4>Contact Us</h4>
-              <p>Email: blaekmot@gmail.com</p>
-              <p>Phone: +855 96 272 8396</p>
-              <p>LinkedIn: RATANAKKHOEURN</p>
-            </div>
-            <div class="col-md mb-5">
-              <h4>Service</h4>
-              <p>Email: blaekmot@gmail.com</p>
-              <p>Phone: +855 96 272 8396</p>
-              <p>LinkedIn: RATANAKKHOEURN</p>
-            </div>
-            <div class="col-md">
-              <h4>Follow Us</h4>
-              <input
-                type="text"
-                class="form-control p-1 w-80"
-                placeholder="What would like to eat?"
-                aria-label="Search"
-              />
-              <ul class="list-unstyled d-flex justify-content-md-start mt-3 gap-2">
-                <li class="ms-3">
-                  <a href="#" class="socialIcon"
-                    ><img
-                      src="../../assets/FooterImages/facebook.png"
-                      alt="Facebook"
-                      class="img-fluid"
-                      style="width: 35px; height: 35px; border-radius: 100%"
-                  /></a>
-                </li>
-                <li class="ms-3">
-                  <a href="#" class="socialIcon"
-                    ><img
-                      src="../../assets/FooterImages/twitter.png"
-                      alt="Twitter"
-                      class="img-fluid"
-                      style="width: 35px; height: 35px; border-radius: 100%"
-                  /></a>
-                </li>
-                <li class="ms-3">
-                  <a href="#" class="socialIcon"
-                    ><img
-                      src="../../assets/FooterImages/instagram.png"
-                      alt="Instagram"
-                      class="img-fluid"
-                      style="width: 35px; height: 35px; border-radius: 100%"
-                  /></a>
-                </li>
-                <li class="ms-3">
-                  <a href="#" class="socialIcon"
-                    ><img
-                      src="../../assets/FooterImages/linkedIn.png"
-                      alt="LinkedIn"
-                      class="img-fluid"
-                      style="width: 35px; height: 35px; border-radius: 100%"
-                  /></a>
-                </li>
-                <li class="ms-3">
-                  <a href="#" class="socialIcon"
-                    ><img
-                      src="../../assets/FooterImages/youtube.png"
-                      alt="YouTube"
-                      class="img-fluid"
-                      style="width: 35px; height: 35px; border-radius: 100%"
-                  /></a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </footer>
-    </div>
+    <FooterView></FooterView>
   </div>
 </template>
+
+<script>
+import NavbarView from './Navbar/NavbarView.vue'
+import FooterView from './Footer/FooterView.vue'
+export default {
+  components: {
+    NavbarView,
+    FooterView,
+  }
+}
+</script>
