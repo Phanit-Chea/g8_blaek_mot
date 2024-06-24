f4144c6c883c7a9b0e4fa963bdd4e4914e82353b
PHANIT.CHEA <PHANIT.CHEA@PNC.LAN>
2024-06-24 13:47:12 +0700

    resolve conflict

--- frontend/src/Components/FoodCard.vue
+++ frontend/src/Components/FoodCard.vue
@@ -2,100 +2,66 @@
   <div class="col py-3">
     <div class="container mx-auto mt-4">
       <div class="row d-flex">
-<<<<<<< HEAD
-        <div class="card ms-4" style=" width:300px">
+        <div class="card ms-4" style=" width:22.5%">
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiXr8dJVZ4N_-7d5cwXGGLXWzs_esjltt0Dw&s"
             class="card-img" alt="..." />
-          <div class="card-body d-flex justify-content-between">
+          <div class="card-body d-flex justify-content-between px-0">
             <h4 class="card-title text-dark ">Card title</h4>
             <a href="#" class="btn " style="background-color: #54983c">Detail</a>
           </div>
         </div>
-        <div class="card ms-4" style=" width:300px">
+        <div class="card ms-4" style=" width:22.5%">
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiXr8dJVZ4N_-7d5cwXGGLXWzs_esjltt0Dw&s"
             class="card-img" alt="..." />
-          <div class="card-body d-flex justify-content-between">
+          <div class="card-body d-flex justify-content-between px-0">
             <h4 class="card-title text-dark ">Card title</h4>
             <a href="#" class="btn " style="background-color: #54983c">Detail</a>
           </div>
         </div>
-        <div class="card ms-4" style=" width:300px">
+        <div class="card ms-4" style=" width:22.5%">
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiXr8dJVZ4N_-7d5cwXGGLXWzs_esjltt0Dw&s"
             class="card-img" alt="..." />
-          <div class="card-body d-flex justify-content-between">
+          <div class="card-body d-flex justify-content-between px-0">
             <h4 class="card-title text-dark ">Card title</h4>
             <a href="#" class="btn " style="background-color: #54983c">Detail</a>
           </div>
         </div>
-        <div class="card ms-4" style=" width:300px">
+        <div class="card ms-4" style=" width:22.5%">
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiXr8dJVZ4N_-7d5cwXGGLXWzs_esjltt0Dw&s"
             class="card-img" alt="..." />
-          <div class="card-body d-flex justify-content-between">
+          <div class="card-body d-flex justify-content-between px-0">
             <h4 class="card-title text-dark ">Card title</h4>
             <a href="#" class="btn " style="background-color: #54983c">Detail</a>
           </div>
         </div>
-        <div class="card ms-4" style=" width:300px">
+        <div class="card ms-4" style=" width:22.5%">
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiXr8dJVZ4N_-7d5cwXGGLXWzs_esjltt0Dw&s"
             class="card-img" alt="..." />
-          <div class="card-body d-flex justify-content-between">
+          <div class="card-body d-flex justify-content-between px-0">
             <h4 class="card-title text-dark ">Card title</h4>
-            <a href="#" class="btn " style="background-color: #54983c"> Detail</a>
-=======
-          <div class="card ms-4" style=" width:22.5%">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiXr8dJVZ4N_-7d5cwXGGLXWzs_esjltt0Dw&s" class="card-img" alt="..." />
-            <div class="card-body d-flex justify-content-between px-0">
-              <h4 class="card-title text-dark ">Card title</h4>
-              <a href="#" class="btn " style="background-color: #54983c">Detail</a>
-            </div>
+            <a href="#" class="btn " style="background-color: #54983c">Detail</a>
           </div>
-          <div class="card ms-4" style=" width:22.5%">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiXr8dJVZ4N_-7d5cwXGGLXWzs_esjltt0Dw&s" class="card-img" alt="..." />
-            <div class="card-body d-flex justify-content-between px-0">
-              <h4 class="card-title text-dark ">Card title</h4>
-              <a href="#" class="btn " style="background-color: #54983c">Detail</a>
-            </div>
+        </div>
+        <div class="card ms-4" style=" width:22.5%">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiXr8dJVZ4N_-7d5cwXGGLXWzs_esjltt0Dw&s"
+            class="card-img" alt="..." />
+          <div class="card-body d-flex justify-content-between px-0">
+            <h4 class="card-title text-dark ">Card title</h4>
+            <a href="#" class="btn " style="background-color: #54983c">Detail</a>
           </div>
-          <div class="card ms-4" style=" width:22.5%">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiXr8dJVZ4N_-7d5cwXGGLXWzs_esjltt0Dw&s" class="card-img" alt="..." />
-            <div class="card-body d-flex justify-content-between px-0">
-              <h4 class="card-title text-dark ">Card title</h4>
-              <a href="#" class="btn " style="background-color: #54983c">Detail</a>
-            </div>
-          </div>
-          <div class="card ms-4" style=" width:22.5%">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiXr8dJVZ4N_-7d5cwXGGLXWzs_esjltt0Dw&s" class="card-img" alt="..." />
-            <div class="card-body d-flex justify-content-between px-0">
-              <h4 class="card-title text-dark ">Card title</h4>
-              <a href="#" class="btn " style="background-color: #54983c">Detail</a>
-            </div>
-          </div>
-          <div class="card ms-4" style=" width:22.5%">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiXr8dJVZ4N_-7d5cwXGGLXWzs_esjltt0Dw&s" class="card-img" alt="..." />
-            <div class="card-body d-flex justify-content-between px-0">
-              <h4 class="card-title text-dark ">Card title</h4>
-              <a href="#" class="btn " style="background-color: #54983c">Detail</a>
-            </div>
-          </div>
-          <div class="card ms-4" style=" width:22.5%">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiXr8dJVZ4N_-7d5cwXGGLXWzs_esjltt0Dw&s" class="card-img" alt="..." />
-            <div class="card-body d-flex justify-content-between px-0">
-              <h4 class="card-title text-dark ">Card title</h4>
-              <a href="#" class="btn " style="background-color: #54983c">Detail</a>
-            </div>
-          </div>
-          <div class="card ms-4" style=" width:22.5%">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiXr8dJVZ4N_-7d5cwXGGLXWzs_esjltt0Dw&s" class="card-img" alt="..." />
-            <div class="card-body d-flex justify-content-between px-0">
-              <h4 class="card-title text-dark ">Card title</h4>
-              <a href="#" class="btn " style="background-color: #54983c">Detail</a>
-            </div>
->>>>>>> category
+        </div>
+        <div class="card ms-4" style=" width:22.5%">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiXr8dJVZ4N_-7d5cwXGGLXWzs_esjltt0Dw&s"
+            class="card-img" alt="..." />
+          <div class="card-body d-flex justify-content-between px-0">
+            <h4 class="card-title text-dark ">Card title</h4>
+            <a href="#" class="btn " style="background-color: #54983c">Detail</a>
           </div>
         </div>
       </div>
     </div>
   </div>
+
 </template>
 
 <script>
