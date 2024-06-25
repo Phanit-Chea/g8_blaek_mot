102df5d7abf3a2ca2a596d13dd3a96437324ce97
Your Name <you@example.com>
2024-06-25 14:26:57 +0700

    resold code conflict

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
-<script setup lang="ts">
-
-// import FoodDetail from './views/Web/FoodDetail/FoodDetail.vue';
-</script>
-
-<template>
-  <RouterView />
-
-=======
 <template>
   <RouterView />
   <NavbarView></NavbarView>
   <FooterView></FooterView>
->>>>>>> ecaf23b82163045c2a15ec070014612b1cd0b6e9
 </template>
 <script>
 import NavbarView from "./views/Web/Navbar/NavbarView.vue"
