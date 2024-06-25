f94818e0f0a0756be81e5b1e4acc3c8c695cfd22
PHANIT.CHEA <PHANIT.CHEA@PNC.LAN>
2024-06-25 16:25:31 +0700

    user register #GBM-32

---
+++ backend/app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        dd(1);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+    }
+    public function register(Request $request)
+    {
+        // Validate the incoming request data
+        $validator = Validator::make($request->all(), [
+            'phone_number' => 'required|string',
+            'gender' => 'required|string',
+            'province' => 'required|string',
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+            'profile' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+        ]);
+        $user_exist = User::where('email', $request->email)->first();
+        if ($user_exist) {
+            return response([
+                'message' => 'User already exist !',
+                'success' => false
+            ], 400);
+        }
+
+
+        $img = $request->profile;
+        $ext = $img->getClientOriginalExtension();
+        $imageName = time() . '.' . $ext;
+        $img->move(public_path('/images/'), $imageName);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone_number' => $request->phone_number,
+            'gender' => $request->gender,
+            'province' => $request->province,
+            'password' => Hash::make($request->password),
+            'profile' => $imageName
+        ]);
+        return response([
+            'message' => 'User created successfully',
+            'success' => true,
+            'user' => $user
+        ], 200);
+    }
+}
