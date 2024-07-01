5438ebdcd0dae712ff14939979be05fce4a01f5d
Your Name <you@example.com>
2024-07-01 08:48:16 +0700

    GBM-53-GBM-54-GBM-55-GBM-56 Code on List all chat with other users, create chats, update chat, and delete chat

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\api\AuthController as ApiAuthController;
 use App\Http\Controllers\Api\FoodController;
 use App\Http\Controllers\API\PostController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ChatController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -34,4 +35,11 @@ Route::post('/register', [ApiAuthController::class, 'register']);
 
 Route::prefix("food")->group(function(){
     Route::post('/create',[FoodController::class,'store'])->name('food.create');
+});
+Route::prefix("chat")->group(function(){
+    Route::post('/create/{to_user}',[ChatController::class,'store'])->name('chat.create')->middleware('auth:sanctum');
+    Route::get('/list',[ChatController::class,'index'])->name('chat.list');
+    Route::get('/{to_user}', [ChatController::class,'show'])->name('chat.show')->middleware('auth:sanctum');
+    Route::put('/update/{id}',[ChatController::class,'update'])->name('chat.update')->middleware('auth:sanctum');
+    Route::delete('/delete/{id}',[ChatController::class,'destroy'])->name('chat.destroy')->middleware('auth:sanctum');
 });
