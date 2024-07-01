2622e4d91774615ecde586826e05cd88a3e76edb
chuonveasna <veasna.chuon@student.passerellesnumeriques.org>
2024-07-01 11:24:13 +0700

    [Update develope #GBM-33]' ;

--- frontend/src/views/Web/Navbar/NavbarView.vue
+++ frontend/src/views/Web/Navbar/NavbarView.vue
@@ -4,20 +4,21 @@
       <img
         src="../../../assets/ContainerImages/logo.png"
         alt="homeImage"
-        style="height: 100px; width: auto"
+        width="90%"
+        height="90px"
       />
     </div>
     <div
       class="navCenter col-md d-flex align-items-center"
-      style="width: 100%; margin-left: 3%; margin-top: 10px; margin-bottom: 10px"
-    >
+      style="width:55%; margin-left: 3%; margin-top: 10px; margin-bottom: 10px"
+       >
       <div class="input-group w-240">
         <button class="input-group-text border border-success border-end-0">
           <i class="material-icons">search</i>
         </button>
         <input
           type="text"
-          class="form-control p-3 border border-success border-end-0 shadow-none"
+          class="form-control p-2 border border-success border-end-0 shadow-none"
           placeholder="What would you like to eat?"
           aria-label="Search"
         />
@@ -32,48 +33,53 @@
     <div class="navRight col-md d-flex justify-content-end align-items-center gap-4 m-2">
       <a
         href="#"
-        class="favoriteIcon me-3 rounded-circle d-flex p-1 justify-content-center align-items-center bg-white text-decoration-none"
+        class="favoriteIcon me-3 rounded-circle d-flex p-0.5 justify-content-center align-items-center bg-white text-decoration-none"
       >
         <i class="material-icons fs-1">turned_in</i>
       </a>
-      <a
-        href="#"
+      
+      <router-link
+        to="#"
         class="notification me-3 rounded-circle d-flex p-1 justify-content-center align-items-center bg-white text-decoration-none"
       >
         <i class="material-icons fs-1">notifications</i>
-      </a>
-      <a
-        v-if="useAuth.isAuthenticated"
-        href="/user"
-        class="account me-3 rounded-circle d-flex p-1 justify-content-center align-items-center bg-white text-decoration-none"
+      </router-link>
+      <router-link v-if="useAuth.isAuthenticated"
+        to="/user"
+        class="account me-3 rounded-circle d-flex justify-content-center align-items-center bg-white text-decoration-none"
+        data-bs-toggle="modal"
+        data-bs-target="#loginForm"
       >
         <img
-           :src="'http://127.0.0.1:8000'+ useAuth.user.profile"
-          alt="profile"
-          width="38px"
-          class="rounded-circle"
+          :src="`http://127.0.0.1:8000${useAuth.user.profile}`"
+          alt="login"
+          width="45px"
+          class="rounded-circle rounded"
+          data-bs-toggle="modal"
+          data-bs-target="#loginForm"
         />
-      </a>
-      <a
-        v-else
-        href="#"
-        class="account me-3 rounded-circle d-flex p-1 justify-content-center align-items-center bg-white text-decoration-none"
+    </router-link>
+      <router-link v-else
+        to="#"
+        class="account me-3 rounded-circle d-flex justify-content-center align-items-center bg-white text-decoration-none"
         data-bs-toggle="modal"
         data-bs-target="#loginForm"
       >
         <img
           src="../../../assets/login.png"
           alt="login"
-          width="38px"
+          width="45px"
+          data-bs-toggle="modal"
+          data-bs-target="#loginForm"
         />
-      </a>
+    </router-link>
     </div>
-    <div class="navPages row text-warning mt-3">
+    <div class="navPages row text-red mt-3">
       <div class="pages col d-flex" style="gap: 20px; margin-left: 20px">
-        <a href="#" class="pagesLink text-green fs-4 text-decoration-none nav-link">Home</a>
-        <a href="#" class="pagesLink text-green fs-4 text-decoration-none nav-link">Category</a>
-        <a href="#" class="pagesLink text-green fs-4 text-decoration-none nav-link">About Us</a>
-        <a href="#" class="pagesLink text-green fs-4 text-decoration-none nav-link">Join With Us</a>
+        <a href="#" class="pagesLink text-green-700 fs-5 text-decoration-none nav-link">Home</a>
+        <a href="#" class="pagesLink text-green-700 fs-5 text-decoration-none nav-link">Category</a>
+        <a href="#" class="pagesLink text-green-700 fs-5 text-decoration-none nav-link">About Us</a>
+        <a href="#" class="pagesLink text-green-700 fs-5 text-decoration-none nav-link">Join With Us</a>
       </div>
     </div>
   </nav>
@@ -90,8 +96,16 @@ const useAuth = useAuthStore();
 .favoriteIcon,
 .notification,
 .account {
-  border: 3px solid rgba(62, 160, 9, 0.942);
-  color: rgba(61, 134, 22, 0.942);
+  transition: transform 0.3s ease, border 0.3s ease;
+  border: 2px solid rgba(62, 160, 9, 0.942);
+  color: rgba(62, 160, 9, 0.942);
+}
+
+.login {
+  display: flex;
+  color: white;
+  background: none;
+  border: none;
 }
 
 .favoriteIcon:hover,
