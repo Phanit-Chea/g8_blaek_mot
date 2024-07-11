f1bfadf809e4aee277e0b82e59d03483a6909063
PHANIT.CHEA <PHANIT.CHEA@PNC.LAN>
2024-07-11 13:39:01 +0700

    [imp update style in admin dashboard #GBM-84]

--- frontend/src/plugins/axios.ts
+++ frontend/src/plugins/axios.ts
@@ -17,26 +17,26 @@ const getImage = axios.create({
 axiosInstance.get('http://127.0.0.1:8000/sanctum/csrf-cookie')
 
 // Add a request interceptor
-axiosInstance.interceptors.request.use(config => {
-  const token = useUserStore.user.remember_token;
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-}, error => {
-  return Promise.reject(error);
-});
+// axiosInstance.interceptors.request.use(config => {
+//   const token = useUserStore.user.remember_token;
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// }, error => {
+//   return Promise.reject(error);
+// });
 
 // Add a response interceptor
-axiosInstance.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    // Handle error
-    if (error.response.status === 401) {
-      // Handle unauthorized access, e.g., redirect to login
-    }
-    return Promise.reject(error)
-  }
-)
+// axiosInstance.interceptors.response.use(
+//   (response) => response,
+//   (error) => {
+//     // Handle error
+//     if (error.response.status === 401) {
+//       // Handle unauthorized access, e.g., redirect to login
+//     }
+//     return Promise.reject(error)
+//   }
+// )
 
 export default axiosInstance
